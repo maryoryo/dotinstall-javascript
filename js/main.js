09c08462607b11adbe8e5484f9a59261d7510c5e
maryoryo <liyomaxxde@gmail.com>
2021-08-16 00:20:29 +0900

    [create]20210815

---
+++ js/main.js
@@ -0,0 +1,125 @@
+'use strict';
+
+// console.log('Hello World from main.js');
+// console.log("it's me");
+// console.log('it\'s me');
+
+// console.log('Hel\nlo Wo\trld');
+
+// console.log('Hello' + 'World');
+
+// let price = 150;
+
+// console.log(price * 140);
+// console.log(price * 160);
+
+
+// price = 170;
+
+// console.log(price * 140);
+// console.log(price * 160);
+
+
+// console.log(parseInt('5, 10')+ 3);
+
+// console.log('5' + 3.to_s);
+
+
+// const price = 1000
+
+
+// const signal = "pink";
+
+// switch (signal) {
+//   case 'red':
+//     console.log('Stop!');
+//     break;
+//   case 'yellow':
+//     console.log('Coution!');
+//     break;
+//   case 'blue':
+//   case 'green':
+//     console.log('Go!');
+//     break;
+//   default:
+//     console.log('wrong signal!')
+//     break;
+// }
+
+
+// for (let i = 1; i <= 10; i++) {
+//   // console.log('hollo');
+//   // console.log('hello + i');
+//   console.log(`hello ${i}`);
+// }
+
+
+// let hp = 100;
+
+// while (hp > 0) {
+//   console.log(`${hp} HP left!`);
+//   hp -= 15;
+// } 
+
+
+// let hp = -50;
+
+// do {
+//   console.log(`${hp} HP left!`);
+//   hp -= 15;
+// } while (hp > 0) 
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i === 4) {
+//   //   if (i % 3 === 0) {
+//   //   continue;
+//   break;
+//   }
+//   console.log(i);
+// }
+
+
+// function sum(a, b, c) {
+//   // console.log(a + b +c);
+//   return a + b + c;
+// }
+
+// const sum = function(a, b, c) {
+//   // console.log(a + b +c);
+//   return a + b + c;
+// };
+
+
+// const sum = (a, b, c) => a + b + c;
+
+// sum(1,2,3);
+// sum(4,5,6);
+
+// const total = sum(1,2,3) + sum(4,5,6);
+// console.log(total);
+
+
+// const double = function(a) {
+//   return a * 2;
+// };
+
+
+// const double = (a) => a * 2;
+// console.log(double(12));
+
+
+// function f() {
+//   const x = 1;
+//   console.log(x);
+// };
+
+// f();
+
+
+
+{
+  const x = 100;
+  console.log(x);
+}
